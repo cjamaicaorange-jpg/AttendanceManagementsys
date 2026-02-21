c60f618023de01604a8f22f967e0ba5682e78831
cjamaicaorange-jpg <cjamaicaorange@gmail.com>
2026-02-21 11:49:12 +0800

    Add files via upload

---
+++ AttendanceManagementSystem/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Data.Common;
+
+namespace AttendanceManagementSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ATTENDACE MANAGEMENT SYSTEM");
+
+            Console.WriteLine("Choices: Record Report View");
+            Console.WriteLine("Enter choice: ");
+            String inputchoice = Console.ReadLine();
+
+            if (inputchoice == "record")
+            {
+                Console.WriteLine("You are Present.");
+            }
+            else if (inputchoice == "report")
+            {
+                Console.WriteLine("Choices: File for Absent\nExit");
+            }
+
+        }
+    }
+}
